d0c3a23183e2ae4bec4883a635a11c4ad01b1264
Egotblin <ikriannikove01@gmail.com>
2023-10-12 14:03:03 +0300

    add_elements_function

--- scripts/db_connection.py
+++ scripts/db_connection.py
@@ -21,4 +21,34 @@ class db_connection():
             cls.connection = psycopg2.connect(**connect_params)
         return cls.instance
 
+    def add_elements(self, table_name, **kwargs):
+        """
+        Получает имя таблицы и записывает в него значения
 
+        kwargs - словарь, где ключи - названия полей, значения в них - то, что нужно добавить
+        """
+
+        # создаем список из ключей и их значений
+        keys = []
+        values = []
+
+        for key, value in kwargs.items():
+            keys.append(key)
+            # если тип значения - строка, то добавим кавычки
+            if type(value) is str: 
+                value = f"'{value}'"
+            values.append(value)
+
+        # добавление в таблицу значений
+        cursor = self.connection.cursor()
+        cursor.execute(f"""
+                        INSERT INTO {table_name} ({', '.join(keys)})
+                        VALUES({', '.join(map(str, values))})
+                        """
+        )
+        cursor.close()
+        self.connection.commit()
+
+
+    def del_user(table_name, user_session_hash):
+        pass
