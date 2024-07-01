130d0ba347cf56115922462fda18e090b87f96dc
Egotblin <ikriannikove01@gmail.com>
2024-07-01 17:30:55 +0300

    fix отправка конфига аппаратуры

--- scripts/norm_creation.py
+++ scripts/norm_creation.py
@@ -5,7 +5,7 @@ def get_aparat_config(aparat_name: str):
     """
     :param apparat_name: назваие аппаратуры
     """
-    aparat_configs_folder = './apparat_configs/'
+    aparat_configs_folder = './aparat_configs/'
     apparat_name = apparat_name + '.json'
     apparat_config_path = os.path.join(aparat_configs_folder, aparat_name)
     with open(apparat_config_path, encoding='utf-8') as f:
